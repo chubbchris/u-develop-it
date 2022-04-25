de0e99c32cd4ccebf61751ddbd8136eb70fac19f
chris chubb <chubb91chris@icloud.com>
2022-04-25 19:34:41 -0400

    added crud routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
@@ -15,8 +16,65 @@ const db = mysql.createConnection(
     },
     console.log('connected to election database.')
 );
-db.query(`SELECT * FROM candidates`, (err,rows)=>{
-    console.log(rows);
+app.get('/api/candidates',(req,res)=>{
+    const sql=`SELECT * FROM candidates`;
+ db.query(sql,(err,rows)=>{
+     if(err){
+         res.status(500).json({error: err.message});
+         return;
+     }
+     res.json({
+         message: 'success',
+         data: rows
+     });
+ });
+});
+
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err,row)=>{
+    if (err){
+       console.log(err);
+    }
+    console.log(row);
+});
+app.delete('/api/candidates/:id',(req,res)=>{
+const sql = `DELETE FROM candidates WHERE id = ?`;
+const params = [req.params.id];
+db.query(sql,params,(err,result)=>{
+    if (err){
+        res.statusMessage(400).json({error: res.message});
+    } else if (!result.affectedRows){
+        res.json({
+            Message: 'canidate not found'
+        });
+    } else {
+        res.json({
+            message: 'deleted',
+            changes: result.affectedRows,
+            id: req.params.id
+        });
+    }
+});
+});
+app.post('/api/candidates',({body},res)=>{
+    const errors = inputCheck(body, 'first_name','last_name','industry_connected');
+    if (errors){
+        res.status(400).json({errors:errors});
+        return;
+    }
+    const sql =`INSERT INTO candidates (first_name,last_name,industry_connected)
+    values (?,?,?)`;
+    const params = [body.first_name,body.last_name,body.industry_connected];
+
+    db.query(sql,params,(err,result)=>{
+        if(err){
+            res.status(400).json({error:err.message})
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
 });
 app.use((req,res)=>{
     res.status(404).end();
