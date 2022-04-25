2e99b41aa86d83d4bae19ec9bf9969df4cfa9eeb
chris chubb <chubb91chris@icloud.com>
2022-04-24 22:34:21 -0400

    added sql

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-
+const db = mysql.createConnection(
+    {
+        host: 'localhost' ,
+        user: 'root' ,
+        password: 'chubb1',
+        database: 'election'
+    },
+    console.log('connected to election database.')
+);
+db.query(`SELECT * FROM candidates`, (err,rows)=>{
+    console.log(rows);
+});
 app.use((req,res)=>{
     res.status(404).end();
 });
